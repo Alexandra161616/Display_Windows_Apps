485ac352e68c24b906e04909a1716a2e9d4cb5b3
Alexandra <georgianaalexandrac@gmail.com>
2023-07-06 23:08:16 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Runtime.InteropServices;
+
+namespace mi_e_greu2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //Acceseaza GetInstalledApps in Window Forms Constructor.  
+            GetInstalledApps();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void GetInstalledApps()
+
+        {
+            int distanceUnit = 1;
+
+            string uninstallKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"; //path ul bine cunoscut al aplicatiilor in windows
+
+            using (RegistryKey rk = Registry.LocalMachine.OpenSubKey(uninstallKey))
+
+            {
+                foreach (string skName in rk.GetSubKeyNames())
+                { 
+                    using (RegistryKey sk = rk.OpenSubKey(skName))  //asta permite accesul la subkeys(installed aplication entries)
+                    {
+                        
+
+                        object displayName = sk.GetValue("DisplayName");
+                        if (displayName != null)
+                        {
+
+                            Label lbl = new Label();
+                            lbl.Top = distanceUnit * 30;
+                            lbl.Height = 20;
+                          
+                            distanceUnit += 1;
+                            lbl.Text = displayName.ToString();
+                            panel1.Controls.Add(lbl);
+                            
+                        }
+                    }
+
+                }
+
+
+
+            }
+
+        }
+
+
+
+      
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        //scrii asta ca sa ai consola
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
+        
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("mi e greu2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("mi e greu2")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e73e849b-b177-42c1-917e-6e27cb914c73")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
